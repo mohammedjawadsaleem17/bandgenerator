b3052154dc4c65761609f41c9bab461d3df5d599
Mohammed Jawad Saleem <mohammedjawadsaleem17@gmail.com>
2023-09-22 12:16:58 +0530

    added few changes

--- index.js
+++ index.js
@@ -1,74 +1,65 @@
 let len = document.querySelectorAll(".drum").length;
+console.log(`Length of the Buttons is ${len}.`)
 
-for (let i = 0; i < len; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    let mouse = this.innerHTML;
-    makeSound(mouse);
-    //Based on the key pressed i have to play sound;
-    buttonAnimation(mouse)
-  });
+
+for(let i=0;i<len;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        console.log(this.innerHTML);
+        playSound(this.innerHTML);
+    })
 }
 
 
-document.addEventListener("keydown",function(e){
-    console.log(e.isTrusted);
-    console.log(e.key)
-    makeSound(e.key);
-    buttonAnimation(e.key)
+//Keyboard Press
+document.addEventListener("keydown",function(event){
+    console.log(event.key);
+    playSound(event.key)
 })
 
 
+function playSound(letter){
+    switch(letter){
+        case "w":
+            console.log("a");
+            let crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "s":
+            console.log("s");
+            let snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "a":
+            console.log("a");
+            let kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "d":
+            console.log("d");
+            let tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            console.log("j");
+            let tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            console.log("k");
+            let tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            console.log("l");
+            let tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default:
+            console.log("invalid click");
+            let error = new Audio("./sounds/error.wav");
+            error.play();
 
-
-function makeSound(key) {
-  switch (key) {
-    case "w":
-      console.log("You have pressed W");
-      let crash = new Audio("./sounds/crash.mp3");
-      crash.play();
-      break;
-    case "a":
-      console.log("You have pressed A");
-      let kick = new Audio("./sounds/kick-bass.mp3");
-      kick.play();
-      break;
-    case "s":
-      console.log("You have pressed S");
-      let snare = new Audio("./sounds/snare.mp3");
-      snare.play();
-      break;
-    case "d":
-      console.log("You have pressed S");
-      let tom1 = new Audio("./sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case "j":
-      console.log("you have pressed J");
-      let tom2 = new Audio("./sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "k":
-      console.log("You have pressed K");
-      let tom3 = new Audio("./sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "l":
-      console.log("You have pressed l");
-      let tom4 = new Audio("./sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    default:
-    //   alert("Invalid Request was Submitted");
-  }
-}
-
-function buttonAnimation(event){
-    let button = document.querySelector("."+event).classList.add("pressed");
-
-    setTimeout(unPress,100);
-    function unPress(){
-        document.querySelector("."+event).classList.remove("pressed");
     }
 }
 
-document.querySelector("footer").style.color="black";
+document.querySelector("footer").style.fontVariant="small-caps";
